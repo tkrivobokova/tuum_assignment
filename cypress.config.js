f575f9f8ea9b0604204ea007480067bb8a3f63d6
Tatjana Krivobokova <tkrivobokova@gmail.com>
2024-02-15 10:57:47 +0200

    excluded baseUrl

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,6 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "https://person-api.sandbox.tuumplatform.com",
   },
   defaultCommandTimeout: 10000,
   fixturesFolder: false
