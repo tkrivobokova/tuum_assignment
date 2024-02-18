7f8feefdc6ae3231d770e99030718eb38c2e73d4
Tatjana Krivobokova <tkrivobokova@gmail.com>
2024-02-18 17:14:54 +0200

    created test for 400 status code

--- cypress/e2e/api/createAccountTransaction.cy.js
+++ cypress/e2e/api/createAccountTransaction.cy.js
@@ -75,7 +75,7 @@ describe("Create a new account transaction", () => {
     });
 
     // status code 200
-    it.skip("should return 200 when creating an account transaction", () => {
+    it("should return 200 when creating an account transaction", () => {
         cy.request({
             method: "POST",
             url: "https://account-api.sandbox.tuumplatform.com/api/v4/accounts/" + accountId + "/transactions",
@@ -93,6 +93,392 @@ describe("Create a new account transaction", () => {
                 expect(item.amount).to.equal(1.22);
                 expect(item.accountTransactionId).not.null;
             });
+        });
+    });
+
+    // status code 400
+    it("should return 400 if payload is missing", () => {
+        cy.request({
+            method: "POST",
+            url: "https://account-api.sandbox.tuumplatform.com/api/v4/accounts/" + accountId + "/transactions",
+            headers: headers,
+            failOnStatusCode: false
+        }).then((response) => {
+            expect(response.status).to.equal(400);
+            expect(response.body).to.have.property("errors");
+            expect(response.body).to.have.property("validationErrors");
+        });
+    });
+
+    it("should return 400 if payload is empty object", () => {
+        cy.request({
+            method: "POST",
+            url: "https://account-api.sandbox.tuumplatform.com/api/v4/accounts/" + accountId + "/transactions",
+            headers: headers,
+            failOnStatusCode: false,
+            body: emptyObject400
+        }).then((response) => {
+            expect(response.status).to.equal(400);
+            expect(response.body.errors).to.include("err.validationErrors");
+            expect(response.body).to.have.property("validationErrors");
+        });
+    });
+
+    it("should return 400 if transactionTypeCode is missing", () => {
+        cy.request({
+            method: "POST",
+            url: "https://account-api.sandbox.tuumplatform.com/api/v4/accounts/" + accountId + "/transactions",
+            headers: headers,
+            failOnStatusCode: false,
+            body: transactionTypeCodeMissing400
+        }).then((response) => {
+            const error = response.body.validationErrors.find(error => error.field === "transactionTypeCode");
+            const errorCode = error.errors.find(error => error.code === "err.notBlank");
+
+            expect(response.status).to.equal(400);
+            expect(response.body.errors).to.include("err.validationErrors");
+            expect(response.body).to.have.property("validationErrors");
+            expect(error).to.exist;
+            expect(errorCode).to.exist;
+
+        });
+    });
+
+    it("should return 400 if transactionTypeCode is empty", () => {
+        cy.request({
+            method: "POST",
+            url: "https://account-api.sandbox.tuumplatform.com/api/v4/accounts/" + accountId + "/transactions",
+            headers: headers,
+            failOnStatusCode: false,
+            body: transactionTypeCodeEmpty400
+        }).then((response) => {
+            const error = response.body.validationErrors.find(error => error.field === "transactionTypeCode");
+            const errorCode = error.errors.find(error => error.code === "err.notBlank");
+
+            expect(response.status).to.equal(400);
+            expect(response.body.errors).to.include("err.validationErrors");
+            expect(response.body).to.have.property("validationErrors");
+            expect(error).to.exist;
+            expect(errorCode).to.exist;
+
+        });
+    });
+
+    it("should return 400 if transactionTypeCode contains only spaces", () => {
+        cy.request({
+            method: "POST",
+            url: "https://account-api.sandbox.tuumplatform.com/api/v4/accounts/" + accountId + "/transactions",
+            headers: headers,
+            failOnStatusCode: false,
+            body: transactionTypeCodeSpace400
+        }).then((response) => {
+            const error = response.body.validationErrors.find(error => error.field === "transactionTypeCode");
+            const errorCode = error.errors.find(error => error.code === "err.notBlank");
+
+            expect(response.status).to.equal(400);
+            expect(response.body.errors).to.include("err.validationErrors");
+            expect(response.body).to.have.property("validationErrors");
+            expect(error).to.exist;
+            expect(errorCode).to.exist;
+
+        });
+    });
+
+    it("should return 400 if transactionTypeCode is null", () => {
+        cy.request({
+            method: "POST",
+            url: "https://account-api.sandbox.tuumplatform.com/api/v4/accounts/" + accountId + "/transactions",
+            headers: headers,
+            failOnStatusCode: false,
+            body: transactionTypeCodeNull400
+        }).then((response) => {
+            const error = response.body.validationErrors.find(error => error.field === "transactionTypeCode");
+            const errorCode = error.errors.find(error => error.code === "err.notBlank");
+
+            expect(response.status).to.equal(400);
+            expect(response.body.errors).to.include("err.validationErrors");
+            expect(response.body).to.have.property("validationErrors");
+            expect(error).to.exist;
+            expect(errorCode).to.exist;
+
+        });
+    });
+
+    it("should return 400 if transactionTypeCode is invalid", () => {
+        cy.request({
+            method: "POST",
+            url: "https://account-api.sandbox.tuumplatform.com/api/v4/accounts/" + accountId + "/transactions",
+            headers: headers,
+            failOnStatusCode: false,
+            body: transactionTypeCodeInvalid400
+        }).then((response) => {
+            expect(response.status).to.equal(400);
+            expect(response.body.errors).to.include("err.transactionTypeNotFound");
+            expect(response.body).to.have.property("validationErrors");
+        });
+    });
+
+    it("should return 400 if money is missing", () => {
+        cy.request({
+            method: "POST",
+            url: "https://account-api.sandbox.tuumplatform.com/api/v4/accounts/" + accountId + "/transactions",
+            headers: headers,
+            failOnStatusCode: false,
+            body: moneyMissing400
+        }).then((response) => {
+            const error = response.body.validationErrors.find(error => error.field === "money");
+            const errorCode = error.errors.find(error => error.code === "err.notNull");
+
+            expect(response.status).to.equal(400);
+            expect(response.body.errors).to.include("err.validationErrors");
+            expect(response.body).to.have.property("validationErrors");
+            expect(error).to.exist;
+            expect(errorCode).to.exist;
+
+        });
+    });
+
+    it("should return 400 if money is empty", () => {
+        cy.request({
+            method: "POST",
+            url: "https://account-api.sandbox.tuumplatform.com/api/v4/accounts/" + accountId + "/transactions",
+            headers: headers,
+            failOnStatusCode: false,
+            body: moneyEmpty400
+        }).then((response) => {
+            const error1 = response.body.validationErrors.find(error => error.field === "money.currencyCode" && error.errors.some(e => e.code === "err.notNull"));
+            const error2 = response.body.validationErrors.find(error => error.field === "money.amount" && error.errors.some(e => e.code === "err.notNull"));
+
+            expect(response.status).to.equal(400);
+            expect(response.body.errors).to.include("err.validationErrors");
+            expect(response.body).to.have.property("validationErrors");
+            expect(error1).to.exist;
+            expect(error2).to.exist;
+        });
+    });
+
+    it("should return 400 if money_amount is missing", () => {
+        cy.request({
+            method: "POST",
+            url: "https://account-api.sandbox.tuumplatform.com/api/v4/accounts/" + accountId + "/transactions",
+            headers: headers,
+            failOnStatusCode: false,
+            body: money_amountMissing400
+        }).then((response) => {
+            const error = response.body.validationErrors.find(error => error.field === "money.amount");
+            const errorCode = error.errors.find(error => error.code === "err.notNull");
+
+            expect(response.status).to.equal(400);
+            expect(response.body.errors).to.include("err.validationErrors");
+            expect(response.body).to.have.property("validationErrors");
+            expect(error).to.exist;
+            expect(errorCode).to.exist;
+
+        });
+    });
+
+    it("should return 400 if money_amount is empty", () => {
+        cy.request({
+            method: "POST",
+            url: "https://account-api.sandbox.tuumplatform.com/api/v4/accounts/" + accountId + "/transactions",
+            headers: headers,
+            failOnStatusCode: false,
+            body: money_amountEmpty400
+        }).then((response) => {
+            const error = response.body.validationErrors.find(error => error.field === "money.amount");
+            const errorCode = error.errors.find(error => error.code === "err.notNull");
+
+            expect(response.status).to.equal(400);
+            expect(response.body.errors).to.include("err.validationErrors");
+            expect(response.body).to.have.property("validationErrors");
+            expect(error).to.exist;
+            expect(errorCode).to.exist;
+
+        });
+    });
+
+    it("should return 400 if money_amount contains only spaces", () => {
+        cy.request({
+            method: "POST",
+            url: "https://account-api.sandbox.tuumplatform.com/api/v4/accounts/" + accountId + "/transactions",
+            headers: headers,
+            failOnStatusCode: false,
+            body: money_amountSpace400
+        }).then((response) => {
+            const error = response.body.validationErrors.find(error => error.field === "money.amount");
+            const errorCode = error.errors.find(error => error.code === "err.notNull");
+
+            expect(response.status).to.equal(400);
+            expect(response.body.errors).to.include("err.validationErrors");
+            expect(response.body).to.have.property("validationErrors");
+            expect(error).to.exist;
+            expect(errorCode).to.exist;
+
+        });
+    });
+
+    it("should return 400 if money_amount is null", () => {
+        cy.request({
+            method: "POST",
+            url: "https://account-api.sandbox.tuumplatform.com/api/v4/accounts/" + accountId + "/transactions",
+            headers: headers,
+            failOnStatusCode: false,
+            body: money_amountNull400
+        }).then((response) => {
+            const error = response.body.validationErrors.find(error => error.field === "money.amount");
+            const errorCode = error.errors.find(error => error.code === "err.notNull");
+
+            expect(response.status).to.equal(400);
+            expect(response.body.errors).to.include("err.validationErrors");
+            expect(response.body).to.have.property("validationErrors");
+            expect(error).to.exist;
+            expect(errorCode).to.exist;
+        });
+    });
+
+    it("should return 400 if money_amount is negative number", () => {
+        cy.request({
+            method: "POST",
+            url: "https://account-api.sandbox.tuumplatform.com/api/v4/accounts/" + accountId + "/transactions",
+            headers: headers,
+            failOnStatusCode: false,
+            body: money_amountNegative400
+        }).then((response) => {
+            const error = response.body.validationErrors.find(error => error.field === "money.amount");
+            const errorCode = error.errors.find(error => error.code === "err.mustBePositive");
+
+            expect(response.status).to.equal(400);
+            expect(response.body.errors).to.include("err.validationErrors");
+            expect(response.body).to.have.property("validationErrors");
+            expect(error).to.exist;
+            expect(errorCode).to.exist;
+        });
+    });
+
+    it("should return 400 if money_amount is invalid", () => {
+        cy.request({
+            method: "POST",
+            url: "https://account-api.sandbox.tuumplatform.com/api/v4/accounts/" + accountId + "/transactions",
+            headers: headers,
+            failOnStatusCode: false,
+            body: money_amountInvalid400
+        }).then((response) => {expect(response.status).to.equal(400);
+            expect(response.body.errors).to.include("Invalid request field: money.amount");
+            expect(response.body).to.have.property("validationErrors");
+        });
+    });
+
+    it("should return 400 if money_amount is too large number", () => {
+        cy.request({
+            method: "POST",
+            url: "https://account-api.sandbox.tuumplatform.com/api/v4/accounts/" + accountId + "/transactions",
+            headers: headers,
+            failOnStatusCode: false,
+            body: money_amountTooLarge400
+        }).then((response) => {
+            const error = response.body.validationErrors.find(error => error.field === "money.amount");
+            const errorCode = error.errors.find(error => error.code === "err.invalidPrecision");
+
+            expect(response.status).to.equal(400);
+            expect(response.body.errors).to.include("err.validationErrors");
+            expect(response.body).to.have.property("validationErrors");
+            expect(error).to.exist;
+            expect(errorCode).to.exist;
+        });
+    });
+
+    it("should return 400 if money_currencyCode is missing", () => {
+        cy.request({
+            method: "POST",
+            url: "https://account-api.sandbox.tuumplatform.com/api/v4/accounts/" + accountId + "/transactions",
+            headers: headers,
+            failOnStatusCode: false,
+            body: money_currencyCodeMissing400
+        }).then((response) => {
+            const error = response.body.validationErrors.find(error => error.field === "money.currencyCode");
+            const errorCode = error.errors.find(error => error.code === "err.notNull");
+
+            expect(response.status).to.equal(400);
+            expect(response.body.errors).to.include("err.validationErrors");
+            expect(response.body).to.have.property("validationErrors");
+            expect(error).to.exist;
+            expect(errorCode).to.exist;
+        });
+    });
+
+    it("should return 400 if money_currencyCode is empty", () => {
+        cy.request({
+            method: "POST",
+            url: "https://account-api.sandbox.tuumplatform.com/api/v4/accounts/" + accountId + "/transactions",
+            headers: headers,
+            failOnStatusCode: false,
+            body: money_currencyCodeEmpty400
+        }).then((response) => {
+            const error = response.body.validationErrors.find(error => error.field === "money.currencyCode");
+            const errorCode = error.errors.find(error => error.code === "err.notNull");
+
+            expect(response.status).to.equal(400);
+            expect(response.body.errors).to.include("err.validationErrors");
+            expect(response.body).to.have.property("validationErrors");
+            expect(error).to.exist;
+            expect(errorCode).to.exist;
+        });
+    });
+
+    it("should return 400 if money_currencyCode contains only spaces", () => {
+        cy.request({
+            method: "POST",
+            url: "https://account-api.sandbox.tuumplatform.com/api/v4/accounts/" + accountId + "/transactions",
+            headers: headers,
+            failOnStatusCode: false,
+            body: money_currencyCodeSpace400
+        }).then((response) => {
+            const error = response.body.validationErrors.find(error => error.field === "money.currencyCode");
+            const errorCode = error.errors.find(error => error.code === "err.notNull");
+
+            expect(response.status).to.equal(400);
+            expect(response.body.errors).to.include("err.validationErrors");
+            expect(response.body).to.have.property("validationErrors");
+            expect(error).to.exist;
+            expect(errorCode).to.exist;
+        });
+    });
+
+    it("should return 400 if money_currencyCode is null", () => {
+        cy.request({
+            method: "POST",
+            url: "https://account-api.sandbox.tuumplatform.com/api/v4/accounts/" + accountId + "/transactions",
+            headers: headers,
+            failOnStatusCode: false,
+            body: money_currencyCodeNull400
+        }).then((response) => {
+            const error = response.body.validationErrors.find(error => error.field === "money.currencyCode");
+            const errorCode = error.errors.find(error => error.code === "err.notNull");
+
+            expect(response.status).to.equal(400);
+            expect(response.body.errors).to.include("err.validationErrors");
+            expect(response.body).to.have.property("validationErrors");
+            expect(error).to.exist;
+            expect(errorCode).to.exist;
+        });
+    });
+
+    it("should return 400 if money_currencyCode is invalid", () => {
+        cy.request({
+            method: "POST",
+            url: "https://account-api.sandbox.tuumplatform.com/api/v4/accounts/" + accountId + "/transactions",
+            headers: headers,
+            failOnStatusCode: false,
+            body: money_currencyCodeInvalid400
+        }).then((response) => {
+            const error = response.body.validationErrors.find(error => error.field === "money.currencyCode");
+            const errorCode = error.errors.find(error => error.code === "err.invalidCurrencyCode");
+
+            expect(response.status).to.equal(400);
+            expect(response.body.errors).to.include("err.validationErrors");
+            expect(response.body).to.have.property("validationErrors");
+            expect(error).to.exist;
+            expect(errorCode).to.exist;
         });
     });
 
